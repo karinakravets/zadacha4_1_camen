31c512281a83392be1171b3879638924a598194d
karina <135805670+karinakravets@users.noreply.github.com>
2023-10-07 13:00:57 +0300

    работает

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,27 @@
- import kotlin.random.Random
-fun main() { val choices = arrayOf("камень", "ножницы", "бумага")
-    val compChoice = choices[Random.nextInt(choices.size)]
-    println("Выбор компьютера: $compChoice")
-    println("Сделайте свой выбор: 1 - камень, 2 - ножницы, 3 - бумага")
-    val playerChoice = when (readLine()) { "1" -> "камень" "2" -> "ножницы" "3" -> "бумага"
-        else -> { println("Неверный ввод. Попробуйте еще раз.")
-            return
+import kotlin.random.Random
+
+    fun main() {
+        val choices = arrayOf("камень", "ножницы", "бумага")
+        val computerChoice = choices[Random.nextInt(choices.size)]
+
+        println("Сделайте свой выбор: 1 - камень, 2 - ножницы, 3 - бумага")
+        val playerChoice = when (readLine()) {
+            "1" -> "камень"
+            "2" -> "ножницы"
+            "3" -> "бумага"
+            else -> {
+                println("Неверный ввод. Попробуйте еще раз.")
+                return
+            }
+        }
+        println("Выбор компьютера: $computerChoice")
+        println("Ваш выбор: $playerChoice")
+
+        when {
+            computerChoice == playerChoice -> println("Ничья, игра переигрывается")
+            computerChoice == "камень" && playerChoice == "ножницы" -> println("Победил компьютер")
+            computerChoice == "ножницы" && playerChoice == "бумага" -> println("Победил компьютер")
+            computerChoice == "бумага" && playerChoice == "камень" -> println("Победил компьютер")
+            else -> println("Вы победили!")
         }
     }
-    println("Ваш выбор: $playerChoice")
-    when { compChoice == playerChoice -> println("Ничья, игра проводится заново")
-        compChoice == "камень" && playerChoice == "ножницы"
-    -> println("Победил компьютер")
-        compChoice == "ножницы" && playerChoice == "бумага"
-    -> println("Победил компьютер")
-        compChoice == "бумага" && playerChoice == "камень"
-        -> println("Победил компьютер")
-        else -> println("Вы победили!")
-    }
-}
-
